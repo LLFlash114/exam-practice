570aeaacb677b5a2ca9612dad2cedfabfc45d63d
Rajveer Singh <singhrajveer4021@gmail.com>
2022-12-08 00:28:43 -0800

    more changes idk

--- week13ex1-main/javascript/script.js
+++ week13ex1-main/javascript/script.js
@@ -1,12 +1,34 @@
-document.body.addEventListener("click", doSomething)
-
 function doSomething(e) {
-    if (e.target.textContent == 'Change Background Color') {
-        changeBGColor()
-    }
+  if (e.target.textContent === "Change Background Color") {
+    changeBGColor();
+  } else if (e.target.textContent === "Submit") {
+    submitHandler();
+  }
 }
 
-// Change background color // 
-function changeBGColor () {
-    document.body.classList.toggle("new-BG-Color")
+// Change background color //
+function changeBGColor() {
+  document.body.classList.toggle("new-BG-Color");
 }
+
+let students = [];
+
+function submitHandler() {
+  const firstNameInput = document.querySelector("#firstname");
+  const lastNameInput = document.querySelector("#lastname");
+  const progNameInput = document.querySelector("#progname");
+
+
+  let studentObj = {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    progName: progNameInput.value,
+  };
+
+  students.push(studentObj);
+  firstNameInput.value = "";
+  lastNameInput.value = "";
+  progNameInput.value = "";
+}
+
+document.body.addEventListener("click", doSomething);
